cc613c6c5cd544cb45b4f985206ff57b202a4eed
chaturvedi-anil <chaturvedia560@gmail.com>
2025-03-09 23:08:01 +0530

    Added signin and signup end points

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -1,11 +1,34 @@
-import express from "express";
+import express, { json } from "express";
 
 const app = express();
 
-app.listen(3000, (err) => {
+app.use(express.json());
+
+app.get("/ping", (req, res) => {
+    res.json({message: "pong"});
+});
+
+app.post("/signup", (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+});
+
+app.post("/signin", (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+});
+
+
+app.listen(3001, (err) => {
     if (err) {
         console.error(`error in running http server : ${err}`);
     }
 
     console.log(`express server is running on port 3000`);
-});
+});     
