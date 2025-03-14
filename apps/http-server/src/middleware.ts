6ff1c452a3c328ea60e46f89aea5d113ac048db0
chaturvedi-anil <chaturvedia560@gmail.com>
2025-03-15 00:12:14 +0530

    Added auth middleware

---
+++ apps/http-server/src/middleware.ts
@@ -0,0 +1,6 @@
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+
+export const authMiddleware = (req: Request, res:Response, next: NextFunction) => {
+
+}
