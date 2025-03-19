afab12bc326c72b7e35e4aca21f56a1471ff13ab
chaturvedi-anil <chaturvedia560@gmail.com>
2025-03-19 23:39:27 +0530

    Added join and leave chat room feature

--- apps/ws-server/src/index.ts
+++ apps/ws-server/src/index.ts
@@ -1,9 +1,35 @@
 import { WebSocket } from "ws";
 import jwt, { JwtPayload } from "jsonwebtoken";
 import { JWT_SECRET } from "@repo/backend-common/config";
+import { prismaClient } from "@repo/db/prismaClient"
  
 const wss = new WebSocket.Server({port: 8080});
 
+interface User {
+    ws: WebSocket,
+    rooms: string[],
+    userId: string
+}
+
+const users:User[] = [];
+
+const checkUser = (token: string): string | null => {
+    try {
+        const decoded = jwt.verify(token, JWT_SECRET);
+
+        if (typeof decoded == "string")  {
+            return null;
+        }
+
+        if(!decoded || !(decoded as JwtPayload).userId){
+            return null;
+        }
+
+        return decoded.userId;
+    } catch (error) {
+        return null;
+    }
+}
 wss.on("connection", (ws, request) => {
 
     const url = request.url;   //ex:  ws://localhost:3000?token=123123
@@ -16,20 +42,89 @@ wss.on("connection", (ws, request) => {
 
     const queryParams = new URLSearchParams(url.split("?")[1]);
     const token = queryParams.get('token') ?? "";
-    const decoded = jwt.verify(token, JWT_SECRET);
+    const userId = checkUser(token);
 
-    if (typeof decoded == "string")  {
+    if(userId == null){
         ws.close();
         return;
     }
 
-    if(!decoded || !(decoded as JwtPayload).userId){
-        ws.close();
-        return;
-    }
+    users.push({
+        userId,
+        rooms:[],
+        ws
+    });
 
-    ws.on("message", (data) => {
-        ws.send("pong");
+    ws.on("message", async (data) => {
+        try {
+            const parsedData = JSON.parse(data.toString());
+            if (parsedData.type === "ping") {
+                ws.send("pong");
+                return;
+            }
+
+            const roomId = parsedData.roomId;
+
+            if (!roomId) {
+                ws.send(JSON.stringify({message: "roomId is required!!"}));
+                return;    
+            }
+
+            const user = users.find(x => x.ws === ws);
+
+            if (!user) {
+                ws.send(JSON.stringify({message: "Invalid user!"}));
+                return;
+            }
+
+            const isRoomPresent = await prismaClient.room.findUnique({
+                where: {
+                    id: parsedData.roomId,
+                }
+            })
+
+            if (!isRoomPresent) {
+                ws.send(JSON.stringify({message: "Invalid roomId!"}));
+                return;
+            }
+
+            // join new room 
+            if (parsedData.type === "join_room") {
+                user?.rooms.push(parsedData.roomId);
+                const isRoomCreated = user.rooms.find(x => x === roomId);
+                if (isRoomCreated) {
+                    ws.send(JSON.stringify({ message: "Room joined successfully!" }));
+                } else {
+                    ws.send(JSON.stringify({ message: "Failed to join room!" }));
+                    return;
+                }
+            }
+
+             // leave joined room 
+            if (parsedData.type === "leave_room") {
+                user.rooms = user?.rooms.filter(x => x !== parsedData.roomId);
+                ws.send(JSON.stringify({ message: "Room leaved successfully!" }));
+                return;
+            }
+
+            // chat
+            if (parsedData.type === "chat") {
+                users.map((user) => {
+                    if (user.ws !== ws && user.rooms.includes(roomId)) {
+                        user.ws.send(JSON.stringify({
+                            type: "chat",
+                            roomId: roomId,
+                            message: parsedData.message,
+                            
+                        }))
+                    }
+                })
+            }
+
+            
+        } catch (error) {
+            ws.send(JSON.stringify({message: "Invalid message format!!"}));
+        }
     });
     ws.send(`Welcome to the websocket server`);
 });
